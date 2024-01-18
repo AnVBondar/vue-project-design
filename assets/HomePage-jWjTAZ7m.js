9d070d8ba0fbca095c37031e4ee90032cb9ac542
Andrey Bondar <andrey_bondar@outlook.com>
2024-01-18 18:11:57 +0200

    Updates

---
+++ assets/HomePage-jWjTAZ7m.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as c}from"./index-cUksCFD-.js";const t={},r={style:{color:"black"}};function s(a,n){return c(),o("h1",r,"Home Page")}const l=e(t,[["render",s]]);export{l as default};
